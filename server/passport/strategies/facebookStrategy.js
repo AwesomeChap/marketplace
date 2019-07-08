0a44d8cae9434ed88639a68faa8f459540009ec6
Jatin Kumar <jatin15011999@gmail.com>
2019-07-08 17:40:19 +0530

    facebook login check over https

--- server/passport/strategies/facebookStrategy.js
+++ server/passport/strategies/facebookStrategy.js
@@ -4,7 +4,7 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 const facebookStratefgy = new FacebookStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: "http://localhost:8080/auth/facebook/callback",
+  callbackURL: "/auth/facebook/callback",
   profileFields: ['id', 'displayName', 'photos', 'email']
 },
   function (accessToken, refreshToken, profile, done) {
